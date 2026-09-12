189959648cb51c719f56d79998bcf4cefb3fe383
archangelinux <mthrange@gmail.com>
2026-09-12 10:04:17 -0400

    qwen config

--- engine/config.py
+++ engine/config.py
@@ -27,3 +27,20 @@ class GPTConfig:
     n_head: int = 12
     n_embd: int = 768
     bias: bool = True
+
+@dataclass
+class QwenConfig:
+    block_size: int = 32768 #max_pos_embds, RoPE makes long context cheaper
+    vocab_size: int = 151936 #3x bigger
+    n_layer: int = 28 
+    n_head: int = 16
+    n_embd: int = 1024 #hidden_size
+    bias: bool = False
+
+    n_kv_head: int = 8 #GQA number - 16 q-heads share 8 k/v-heads, 2 qs per kv
+    head_dim: int = 128 # 1024/16 = 64, but heads are 128 wide; q projects to 16×128 = 2048, k/v to 8×128 = 1024, o_proj maps 2048 → 1024
+
+    intermediate_size: int = 3072 #MLP width (3x hidden, isntead of 4x hidden in gpt)
+    rms_norm_eps: float = 1e-06
+    rope_theta:float = 1000000.0 # base frequency, constant in angle formula
+    #eos_token_id = 151643
